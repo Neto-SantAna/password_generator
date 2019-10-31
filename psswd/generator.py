126d1bfb4eb01f37032e88ce1292188219d392d5
Neto Sant Ana <eneto.santana@gmail.com>
2019-10-30 23:57:02 -0300

    Initialized index

--- psswd/generator.py
+++ psswd/generator.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 from psswd.auth import login_required
@@ -7,3 +7,20 @@ from psswd.db import get_db
 
 
 bp = Blueprint('generator', __name__)
+
+
+@bp.route('/')
+@login_required
+def index():
+    db = get_db()
+    user_id = session.get('user_id')
+    accounts = db.execute(
+        'SELECT accounts.account as account,'
+        ' accounts.password as password,'
+        ' accounts.last_password as last_password,'
+        ' accounts.updated as updated FROM accounts'
+        ' INNER JOIN users ON users.id = accounts.user_id'
+        ' WHERE user_id = ?', (user_id,)
+    ).fetchall()
+
+    return render_template('generator/index.html', accounts=accounts)
