1ed76ac965e93c0d57eaad27092742952d17678d
Neto-SantAna <eneto.santana@gmail.com>
2019-11-01 16:53:37 -0300

    New_account view added

--- psswd/generator.py
+++ psswd/generator.py
@@ -23,3 +23,39 @@ def index():
     ).fetchall()
 
     return render_template('generator/index.html', accounts=accounts)
+
+
+@bp.route('/new_account', methods=['GET', 'POST'])
+def new_account():
+    if request.method == 'POST':
+        account = request.form.get('account')
+        psswd_min = request.form.get('psswd_min')
+        psswd_max = request.form.get('psswd_max')
+        error = None
+
+        if not account:
+            error = 'Account name required!'
+        elif not psswd_min or not psswd_max:
+            error = 'Set password boundaries!'
+        elif psswd_min < 2 or psswd_max > 25:
+            error = 'Password boundaries must be between 2 and 25!'
+        elif psswd_min > psswd_max:
+            error = 'Password minimum length must be bigger than password maximum length!'
+        elif get_db().execute(
+            'SELECT id FROM accounts WHERE account = ? AND user_id = ?', (account, session.get('user_id'),)
+        ).fetchone() is not None:
+            error = 'Account already registered!'
+        
+        if error is None:
+            password = 'TODO'
+            get_db().execute(
+                'INSERT INTO accounts (user_id, account, password, updated) VALUES (?, ?, ?, ?)',
+                (session.get('user_id'), account, password, 1)
+            )
+            get_db().commit()
+
+            return redirect(url_for('index'))
+        
+        flash(error)
+    
+    return render_template('generator/new_account.html')
